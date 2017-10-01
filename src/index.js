69c289c1345cadefec739e8e3c4d515e1e44af2f
Leif Gensert <leif@leif.io>
2017-10-01 23:05:27 +0200

    feat: make api url configurable
    closes #26

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './main.css';
 import { Main } from './Main.elm';
 
-Main.embed(document.getElementById('root'));
+console.log(process.env)
+
+Main.embed(document.getElementById('root'), {
+  url: process.env.ELM_APP_API_URL,
+});
