9a75a1d7ff002ac46293e3337a01111ea11e3b69
Leif Gensert <leif@leif.io>
2017-10-02 19:10:24 +0200

    chore: invalidate cache on cloudfrond
    closes #32

--- deployment/steps/push_to_cloudfront.sh
+++ deployment/steps/push_to_cloudfront.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# Push to S3 bucket
 aws s3 sync --acl public-read build s3://release-ping-assets
+
+# Invalidate Cache
+aws cloudfront create-invalidation --distribution-id ${AWS_CF_DISTRIBUTION_ID} --paths /index.html
